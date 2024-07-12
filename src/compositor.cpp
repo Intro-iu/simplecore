57857c4b3d97875583bb3e8666bdde81dcd556a0
Intro <intro-iu@outlook.com>
2024-07-13 00:13:25 +0800

    deleted:    src/compositor.cpp 	deleted:    src/compositor.h 	modified:   src/main.cpp

--- src/compositor.cpp
+++
@@ -1,6 +0,0 @@
-#include "compositor.h"
-#include <QDebug>
-
-void Compositor::create() {
-    QWaylandCompositor::create();
-}
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,23 @@
 #include <QWaylandCompositor>
 #include <QDebug>
 
+class Compositor : public QWaylandCompositor
+{
+    Q_OBJECT
+public:
+    Compositor() {
+        create();
+    }
+};
+
 int main(int argc, char **argv) {
     QGuiApplication app(argc, argv);
 
-    QWaylandCompositor *compositor = new QWaylandCompositor;
+    Compositor compositor;
 
-    compositor->create();
+    compositor.create();
 
-    if (!compositor->isCreated()) {
+    if (!compositor.isCreated()) {
         qWarning("Failed to create compositor");
         return 1;
     }
