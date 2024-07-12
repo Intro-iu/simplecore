1e94e76aed6ad5b59939b0dd7bc5395dc506c7e4
Intro <intro-iu@outlook.com>
2024-07-13 01:48:57 +0800

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
-#include <QGuiApplication>
+#include <QCoreApplication>
+#include <QProcess>
 #include "compositor.h"
 
 int main(int argc, char **argv) {
     QCoreApplication app(argc, argv);
 
     Compositor *compositor = new Compositor();
+    if (compositor->isCreated()) {
+        process.start("firefox");
+    }
 
     return app.exec();
 }
